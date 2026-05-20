f59f416362d03b73d228c7dd84105c583246b165
Fernando Akira <kaizen.negocios1@gmail.com>
2026-05-19 23:07:16 -0300

    Create init_database.sql

---
+++ scripts /init_database.sql
@@ -0,0 +1,64 @@
+/*
+=============================================================
+Create Database and Schemas
+=============================================================
+Script Purpose:
+    This script creates a new database named 'DataWarehouse' after checking if it already exists. 
+    If the database exists, it is dropped and recreated. Additionally, the script sets up three schemas 
+    within the database: 'bronze', 'silver', and 'gold'.
+	
+WARNING:
+    Running this script will drop the entire 'DataWarehouse' database if it exists. 
+    All data in the database will be permanently deleted. Proceed with caution 
+    and ensure you have proper backups before running this script.
+*/
+
+--Esse fluxo serve para:
+
+--> **Apagar e recriar um banco de dados com segurança, garantindo que nenhuma conexão esteja usando o banco no momento da exclusão.**
+
+--O processo é:
+
+--1. Verifica se o banco existe.
+--2. Fecha conexões ativas.
+--3. Força acesso exclusivo ao banco.
+--4. Remove o banco.
+--5. Permite recriá-lo “do zero”.
+
+--Muito usado em:
+
+--* ambientes de desenvolvimento;
+--* testes;
+--* laboratórios;
+--* pipelines ETL;
+--* projetos de Data Warehouse.
+
+
+USE master;
+GO
+
+-- Drop and recreate the 'DataWarehouse' database
+IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'DataWarehouse')
+BEGIN
+    ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+    DROP DATABASE DataWarehouse;
+END;
+GO
+
+-- Create the 'DataWarehouse' database
+CREATE DATABASE DataWarehouse;
+GO
+
+USE DataWarehouse;
+GO
+
+-- Create Schemas
+CREATE SCHEMA bronze;
+GO
+
+CREATE SCHEMA silver;
+GO
+
+CREATE SCHEMA gold;
+GO
+
